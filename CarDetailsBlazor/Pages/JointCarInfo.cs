c6d84a3da90b77ca9acc6716498ac0e40d3c37d2
grand <grand@DESKTOP-7KHJ0MN>
2023-03-20 18:10:59 +0000

    Add project files.

---
+++ CarDetailsBlazor/Pages/JointCarInfo.cs
@@ -0,0 +1,20 @@
+﻿namespace CarDetailsBlazor.Pages
+{
+    public class JointInfo
+    {
+
+        public string Name { get; set; }
+        public string Headquarters { get; set; }
+        public int Year { get; set; }
+        public string Manufacturer { get; set; }
+        public double Displacement { get; set; }
+        public int Cylinders { get; set; }
+        public int City { get; set; }
+        public int Highway { get; set; }
+        public int Combined { get; set; }
+
+
+
+
+}
+}
