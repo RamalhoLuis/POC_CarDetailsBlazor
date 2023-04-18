8d930e054112ec908f4180bf74826c9bdfa17115
Luis Ramalho <grandelois@hotmail.com>
2023-04-18 17:16:13 +0100

    Adjusts to Mediatr and Refit.

--- CarDetailsAPI/Queries/GetCarsListQuery.cs
+++ CarDetailsAPI/Queries/GetCarsListQuery.cs
@@ -7,7 +7,6 @@ namespace CarDetailsAPI.Queries
 {
     public class GetCarsListQuery : IRequest<List<Car>> 
     {
-        public string? Search { get; set; }
         public class GetCarsListHandler : IRequestHandler<GetCarsListQuery, List<Car>>
         {
             private readonly IDataContext _carData;
@@ -18,9 +17,9 @@ namespace CarDetailsAPI.Queries
             }
             public Task<List<Car>> Handle(GetCarsListQuery request, CancellationToken cancellationToken)
             {
-                var query = _carData.CarsDb.Where(x => x.Name.Contains(request.Search));
 
-                return Task.FromResult(query.ToList());
+
+                return Task.FromResult(_carData.CarsDb.ToList());
             }
         }
     }
